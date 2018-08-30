753342b5157aaf0f9d833c3851189b1b9e78f275
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2018-08-30 09:58:31 -0400

    Debut ESMS

--- esms/views.py
+++ esms/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from esms.esms_constants import CHOIX_INSTITUT, CHOIX_FINAL, EXPLICATIONS
+from esms.esms_constants import CHOIX_FINAL, EXPLICATIONSFR, EXPLICATIONSEN, CHOIXEN,CHOIXFR
 
 
 # Create your views here.
@@ -8,39 +8,63 @@ from esms.esms_constants import CHOIX_INSTITUT, CHOIX_FINAL, EXPLICATIONS
 
 def Faitinstitution(request, choix='', histoire=''):
     # 'b': {'name': 'Génériques aigus', 'choices': ['c', 'd']},
-    if not choix:
-        choix='z'
-    choisi = CHOIX_INSTITUT[choix]['name']
-    choixs = []
-    historiques = []
-    choixprecedent = histoire + choix
-    dernier=''
-    dernier2=''
-    if len(CHOIX_INSTITUT[choix]['choices']) > 0:
-        choixs = [(c, CHOIX_INSTITUT[c]['name'],EXPLICATIONS[c]) for c in CHOIX_INSTITUT[choix]['choices']]
+
+    if langue == 'FR':
+        CHOIX = CHOIXFR
+        EXPLICATIONS = EXPLICATIONSFR
     else:
-        dernier = choisi
-        dernier2 = CHOIX_FINAL[choixprecedent]
+        CHOIX = CHOIXEN
+        EXPLICATIONS = EXPLICATIONSEN
 
-    for a in histoire:
-        if a == choix:
-            dernier = CHOIX_FINAL[histoire]
+    liste =('z','o','x','qq')
+    if not choix:
+        entrees =  [(c, CHOIX[c]['name'], EXPLICATIONS[c]) for c in liste]
+
+        return render(
+            request,
+            'essaiesms.html',
+                    {'choisi': '',
+                     'choixs': '',
+                     'choixprecedent': '',
+                     'historiques': '',
+                     'dernier': '',
+                     'dernier2': '',
+                     'entrees': entrees,
+                     }
+            )
+    else:
+        choisi = CHOIX[choix]['name']
+        choixs = []
+        historiques = []
+        choixprecedent = histoire + choix
+        dernier=''
+        dernier2=''
+        if len(CHOIX[choix]['choices']) > 0:
+            choixs = [(c, CHOIX[c]['name'],EXPLICATIONS[c]) for c in CHOIX[choix]['choices']]
         else:
-            historiques.append(CHOIX_INSTITUT[a]['name'])
+            dernier = choisi
+            dernier2 = CHOIX_FINAL[choixprecedent]
 
-    if dernier:
-        choisi =''
-        choixs =''
-        choixprecedent =''
+        for a in histoire:
+            if a == choix:
+                dernier = CHOIX_FINAL[histoire]
+            else:
+                historiques.append(CHOIX[a]['name'])
 
-    return render(
-        request,
-        'essaiesms.html',
-        {'choisi': choisi,
-         'choixs': choixs,
-         'choixprecedent': choixprecedent,
-         'historiques':  historiques,
-         'dernier': dernier,
-         'dernier2':dernier2,
-         }
-    )
+        if dernier:
+            choisi =''
+            choixs =''
+            choixprecedent =''
+
+        return render(
+            request,
+            'essaiesms.html',
+                    {'choisi': choisi,
+                     'choixs': choixs,
+                     'choixprecedent': choixprecedent,
+                     'historiques':  historiques,
+                     'dernier': dernier,
+                     'dernier2': dernier2,
+                     'entrees': '',
+                     }
+                )
