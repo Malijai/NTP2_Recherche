12b2af3d5bc7c91e5aee64f1195f7d981c1f657e
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2018-11-24 20:35:43 -0500

    Mise en page du pdf de la feuille de temps

--- dataentry/views/feuillesdetemps.py
+++ dataentry/views/feuillesdetemps.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.conf import settings
-from django.template import loader, Context
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.apps import apps
 from reportlab.pdfgen import canvas
@@ -31,47 +30,70 @@ def fdetemps(request):
     fichier = NOM_FICHIER_PDF
     #    doc = SimpleDocTemplate("/tmp/{}".format(NOM_FICHIER_PDF))
     if request.method == 'POST':
+        y_sign = 615
+        xjour = [
+            [113, 138, 163, 188, 213, 238, 263],
+            [299, 324, 348, 372, 396, 420, 445]
+        ]
+        y_heures = 418
+        y_dates = 440
+
+        somme = 0
+
+        date1 = request.POST.get('date')
+        mois, jour, an = date1.split('/')
+        maintenant = datetime.date(int(an), int(mois), int(jour))
+        semaine = maintenant.strftime("%U")
+        if int(semaine) % 2 > 0:
+            d = str(an) + "-W" + semaine
+            debut = datetime.datetime.strptime(d + '-0', "%Y-W%W-%w")
+        else:
+            semcorr = int(semaine) - 1
+            d = str(an) + "-W" + str(semcorr)
+            debut = datetime.datetime.strptime(d + '-0', "%Y-W%W-%w")
+        fin = debut + datetime.timedelta(days=13)
+        quinzaine = (float(maintenant.strftime("%U")) + 4) / 2
+
+        if int(maintenant.strftime("%U")) % 2 > 0:
+            quinzaine = int(quinzaine + 1)
+        if quinzaine > 26:
+            quinzaine = int(quinzaine - 26)
+
+        date_debut = debut.strftime("%d-%m-%Y")
+        date_fin = fin.strftime("%d-%m-%Y")
+
         doc = Canvas("/tmp/{}".format(fichier), pagesize=letter)
         doc.drawImage(img, x, y, w, h)
         doc.setFont('Helvetica', 10)
         doc.drawString(10, 10, PAGE_INFO)
+        doc.drawString(330, y_sign, request.user.contrat.numeemploye)
         doc.setFont('Helvetica', 12)
-        doc.drawString(70, 750, 'no contrat :')
-        doc.drawString(300, 750, 'budget :')
 
-        doc.drawString(70, 636, 'nom')
-        doc.drawString(90, 615, 'signature')
-        doc.drawString(340, 615, 'no')
-        doc.drawString(420, 615, 'date 1')
-        doc.drawString(515, 615, 'date 2 ')
+        doc.drawString(70, 750, 'No contrat : ' + request.user.contrat.numcontrat)
 
-        doc.drawString(110, 440, 'D1')
-        doc.drawString(135, 440, 'L1')
-        doc.drawString(156, 440, 'Ma1')
-        doc.drawString(182, 440, 'Me1')
-        doc.drawString(213, 440, 'J1')
-        doc.drawString(232, 440, 'Ve1')
-        doc.drawString(260, 440, 'S1')
+        doc.drawString(290, 750, 'No budget : ' + request.user.contrat.numbudget)
 
-        doc.drawString(300, 440, 'D2')
-        doc.drawString(325, 440, 'L2')
-        doc.drawString(343, 440, 'Ma2')
-        doc.drawString(368, 440, 'Me2')
-        doc.drawString(395, 440, 'J2')
-        doc.drawString(415, 440, 'Ve2')
-        doc.drawString(445, 440, 'S2')
-        somme = 0
-        y_heures = 418
-        xjour = [
-            [113, 137, 160, 186, 213, 236, 260],
-            [300, 325, 345, 368, 395, 415, 445]
-        ]
+        doc.drawString(70, 636, request.user.first_name.capitalize() + " " + request.user.last_name.capitalize())
+        if request.user.contrat.signature:
+            signature = ImageReader(request.user.contrat.signature)
+            doc.drawImage(signature, 90, (y_sign - 10), 100, 20)
+
+        doc.drawString(450, 645, 'PAIE : ' + str(quinzaine))
+        doc.drawString(420, y_sign, date_debut)
+        doc.drawString(515, y_sign, date_fin)
+
         for semaine in semaines:
             for jour in range(7):
                 x = xjour[semaine][jour]
-                val = request.POST.get("{}_{}".format(semaine,jour))
+                val = request.POST.get("{}_{}".format(semaine, jour))
                 doc.drawString(x, y_heures, val)
                 somme += float(val)
+                if semaine == 1:
+                    ecart = jour + 7
+                else:
+                    ecart = jour
+                newdate = debut + datetime.timedelta(days=ecart)
+                doc.drawString(x, y_dates, newdate.strftime("%d"))
 
         doc.drawString(515, y_heures, str(somme))
 
@@ -87,19 +109,15 @@ def fdetemps(request):
 
         return render(
         request,
-        'fdt.html',
+            'fdt.html',
             {
-            'jours': {0: {'nom' : "Dimanche", 'semaines': semaines},
+            'jours': {0: {'nom': "Dimanche", 'semaines': semaines},
                     1: {'nom': "Lundi", 'semaines': semaines},
                     2: {'nom': "Mardi", 'semaines': semaines},
                     3: {'nom': "Mercredi", 'semaines': semaines},
                     4: {'nom': "Jeudi", 'semaines': semaines},
                     5: {'nom': "Vendredi", 'semaines': semaines},
-                    6: {'nom' : "Samedi", 'semaines': semaines},
+                    6: {'nom': "Samedi", 'semaines': semaines},
                     }
         }
         )
-
-
-
-
