e198f97f6fcf7a69509f6003d143b649a1fa20aa
Malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2019-10-04 10:22:49 -0400

    passage de esms en sub module

--- esms/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import Ressource, Professionnels, Esms
-# Register your models here.
-
-admin.site.register(Professionnels)
-admin.site.register(Ressource)
-admin.site.register(Esms)
